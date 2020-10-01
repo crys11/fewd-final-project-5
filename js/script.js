d52d953bfa967b3a82b60d7de13767d1dcb9b85b
Crystal <63870889+crys11@users.noreply.github.com>
2020-09-30 22:35:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+console.log("JS Loaded!");
+
+const name = "Marc";
+
+console.log(name);
+
+const hiddenMenu = document.getElementById("blank");
+hiddenMenu.addEventListener("mouseover", showMenu);
+
+function showMenu() {
+  document.getElementById("nav-container-area").classList.add("show");
+}
+
+const shownMenu = document.getElementById("nav-container-area");
+shownMenu.addEventListener("mouseleave", hideMenu);
+// console.log("leave")
+
+function hideMenu() {
+  document.getElementById("nav-container-area").classList.remove("show");
+}
+
+const cont = document.getElementById("grid");
+
+for (let i = 1; i < 596; i++) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "square");
+  div.setAttribute("id", "div" + i);
+  div.style.opacity = "0";
+  div.style.zIndex = "10";
+  div.addEventListener("mouseover", showSquare);
+  function showSquare() {
+    div.style.opacity = "1";
+  }
+  // console.log("clicked");
+  // console.log(div);
+  cont.appendChild(div);
+}
